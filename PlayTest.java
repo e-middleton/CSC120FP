10adfc7366b5f302e2e3dbd2ab8ee81444ad1a43
Emily Middleton <emily.a.middleton@gmail.com>
2024-12-03 10:35:11 -0500

    test for moth eating yarn

--- PlayTest.java
+++ PlayTest.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 
+
 public class PlayTest {
     Play game = new Play(); 
-    Map map = new Map(); //default 2x2 map
-    Player hero = new Player(); //auto sets to Dorothy at 0,0
+    Map map = new Map(); //default 4x4
+    Player hero = new Player(); //auto sets to Dorothy 
 
     //getPerson(s) is what throws the exception for lookAtCharacter()
     @Test(expected=MissingNPCException.class)
@@ -16,4 +17,14 @@ public class PlayTest {
         map.findLocation(hero).getPerson("goblin");
     }
 
+    @Test
+    public void testMothEat(){
+        hero.grab("dk yarn");
+        assertEquals(true, hero.hasYarn()); //player has yarn before walking in
+        String[] command = game.sliceAndDice("walk north");
+        game.walk(command, hero, map); //walking up one into the empty field where a Moth is
+        assertEquals(false, hero.hasYarn()); //hero no longer has the yarn after encountering Moth
+        
+    }
+   
 }
