8cf9ca0600b5178c8f090d6bc746db2d729aab11
Emily Middleton <emily.a.middleton@gmail.com>
2024-12-10 08:45:28 -0500

    increased functionality for posing and answering riddles, able to swap out riddles from .txt file for population

--- TalkingDoor.java
+++ TalkingDoor.java
@@ -3,6 +3,10 @@
  * Currently, only special ability is to pose riddles instead of barter
  */
 public class TalkingDoor extends NPC{
+    private String introduction;
+    private String riddle;
+    private String directionProtected; //which path are they protecting (north, south, east, west)
+    private String answer;
 
     /**
      * Constructor for talking door
@@ -11,27 +15,35 @@ public class TalkingDoor extends NPC{
      * @param x x or column position
      * @param y y or row position
      */
-    public TalkingDoor(String description, String occupation, int x, int y){
+    public TalkingDoor(String description, String occupation, String intro, String riddle, String directionProtected, String answer, int x, int y){
         super(description, occupation, x, y);
+        this.introduction = intro;
+        this.riddle = riddle;
+        this.directionProtected = directionProtected;
+        this.answer = answer;
     }
 
-    /**
-     * method for posing a riddle, overrides the regular intro speech that other npc's have
-     * @param a NPC the intro is being given to
-     */
     @Override
-    public void intro(NPC a){ //overrides the super.intro() 
-        System.out.println("I'm almost a String");
-        System.out.println("but that isn't quite right");
-        System.out.println("for how could a String");
-        System.out.println("keep you warm in the night?");
-        System.out.println("I'm hard here to find,");
-        System.out.println("still harder to use");
-        System.out.println("Here's a hint,");
-        System.out.println("I make up socks");
-        System.out.println("inside of your shoes.");
-        System.out.println();
-        System.out.println("What am I?");
+    public void intro(NPC a){
+        System.out.printf(this.introduction); //print formatted because of how it was written into file
+    }
+
+    @Override
+    public void riddle(){ //overrides the super.intro() 
+        System.out.printf(this.riddle);
+    }
+
+    @Override
+    public boolean unlockDoor(String response, Location location){
+        if(response.equals(this.answer)){ //if the user enters the correct response
+            System.out.println("Very well, the door sighs, you may pass.");
+            System.out.println("A grinding sound of old gears turning bounces around the rock cieling");
+            System.out.println("The heavy metal door... swings open");
+            location.unlockPath(directionProtected);
+            return true;
+        } else {
+            return false; //the riddle was guessed incorrectly
+        }
     }
 
     
