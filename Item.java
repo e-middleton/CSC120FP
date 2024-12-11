30f7c249cad3c096724be781da7b46cc05a3b54c
Emily Middleton <emily.a.middleton@gmail.com>
2024-12-11 10:19:23 -0500

    initial commit

---
+++ Item.java
@@ -0,0 +1,61 @@
+public class Item {
+    private String name;
+    private String response;
+    private String description;
+
+    /**
+     * Default constructor for an item with all attributes
+     * @param name the name of the item, it's id
+     * @param response the response it gives if you try to talk to it
+     * @param description a brief description of the item
+     */
+    public Item(String name, String response, String description){
+        this.name = name;
+        this.response = response;
+        this.description = description;
+    }
+
+    /**
+     * Stub constructor for temporary usage/testing
+     * @param name the name of the item
+     */
+    public Item(String name){
+        this.name = name;
+        this.response = null;
+        this.description = null;
+    }
+    
+    /**
+     * Getter for the name/id of an item
+     * @return the name of the item
+     */
+    public String getName(){
+        return this.name;
+    }
+
+    /**
+     * Getter for the response (if you try to talk or barter) of an object
+     * @return the item's response to talking
+     */
+    public String getResponse(){
+        return this.response;
+    }
+
+    /**
+     * Getter for an item's description
+     * @return description of the item
+     */
+    public String getDescription(){
+        return this.description;
+    }
+
+    
+    /**
+     * To string method for an item, getting relevant information, just the name/id
+     * @return a description of an item with all relevant information
+     */
+    public String toString(){
+        return this.name;
+    }
+
+}
